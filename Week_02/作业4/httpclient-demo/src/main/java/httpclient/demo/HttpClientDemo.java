dba1f89ab46e678d7422e988f16fad09ed6917a2
xingyufei <xingyufei325@126.com>
2020-11-04 21:42:53 +0800

    重构代码结构

--- Week_02/作业4/httpclient-demo/src/main/java/httpclient/demo/HttpClientDemo.java
+++ Week_02/作业4/httpclient-demo/src/main/java/httpclient/demo/HttpClientDemo.java
@@ -1,4 +1,4 @@
-package com.xingyufei.week02;
+package httpclient.demo;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
@@ -12,53 +12,64 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 
 /**
- * 作业4：使用httpclient执行get请求
- *
- * Xingyufei
- *
- * 20201027
- */
-public class HttpClientDemo {
-
-    public static void doGet(){
+ * ClassName: HttpClientDemo
+ * Description: 
+ * Author: xyf
+ * Date: 2020-11-04 21:41
+ * Version: 1.0
+ **/
+public class HttpClientDemo
+{
+    public static void doGet()
+    {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         // 创建Get请求
         HttpGet httpGet = new HttpGet("http://localhost:8088/api/hello");
 
         // 响应模型
         CloseableHttpResponse response = null;
-        try {
+        try
+        {
             // 由客户端执行(发送)Get请求
             response = httpClient.execute(httpGet);
             // 从响应模型中获取响应实体
             HttpEntity responseEntity = response.getEntity();
             System.out.println("响应状态为:" + response.getStatusLine());
-            if (responseEntity != null) {
+            if (responseEntity != null)
+            {
                 System.out.println("响应内容长度为:" + responseEntity.getContentLength());
                 System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
             }
-        } catch (ClientProtocolException e) {
+        } catch (ClientProtocolException e)
+        {
             e.printStackTrace();
-        } catch (ParseException e) {
+        } catch (ParseException e)
+        {
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
-        } finally {
-            try {
+        } finally
+        {
+            try
+            {
                 // 释放资源
-                if (httpClient != null) {
+                if (httpClient != null)
+                {
                     httpClient.close();
                 }
-                if (response != null) {
+                if (response != null)
+                {
                     response.close();
                 }
-            } catch (IOException e) {
+            } catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         doGet();
     }
 }
